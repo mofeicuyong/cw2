bdfaf1c3f1cd590f7f08b1a845b4ce3bd709b6ea
mofeicuyong <2919886718@qq.com>
2021-04-30 15:55:49 +0800

    change a void into int

---
+++ clogic.h
@@ -0,0 +1,19 @@
+
+#ifndef GAMEOFLIFE_H_
+#define GAMEOFLIFE_H_
+
+
+#define CSIZE 10  
+#define maxm 1080
+#define maxn 1080
+
+int grid[maxm][maxn];
+int getNumberOfLiveNeighbors(int x, int y);
+int updateGrid();
+int startgame();
+int drawGrid(int windowHeight,int windowWidth,int M,int N);
+int initSDLWindow(int windowHeight,int windowWidth);
+int quitAll();
+int checkEvents();
+int save();
+#endif 
