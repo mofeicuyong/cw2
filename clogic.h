6258509e1357a12c8558534b2739f48d22c544be
mofeicuyong <2919886718@qq.com>
2021-05-06 17:55:02 +0800

    define false and true

--- clogic.h
+++ clogic.h
@@ -2,11 +2,13 @@
 #ifndef GAMEOFLIFE_H_
 #define GAMEOFLIFE_H_
 
+#include <stdint.h>
+#define FALSE 0
+#define TRUE 1
 
 #define CSIZE 10  
 #define maxm 1080
 #define maxn 1080
-
 int grid[maxm][maxn];
 int getNumberOfLiveNeighbors(int x, int y);
 int updateGrid();
